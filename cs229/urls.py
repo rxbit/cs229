1f2ade61788c10a2d4275b6946f0f226c9e1df6f
Jonah <thewindbluesoul@126.com>
2015-05-29 14:17:21 +0800

    url 设置 v2

--- cs229/urls.py
+++ cs229/urls.py
@@ -7,5 +7,5 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     # url(r'^admin/', include(admin.site.urls)),
-    url(r'^/', include('web.urls', namespace='cs229')),
+    url(r'^$', include('web.urls', namespace='cs229')),
 )
