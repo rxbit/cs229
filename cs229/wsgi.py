1618e7114ad649430a57f2541dae70be3758ac28
Jonah <thewindbluesoul@126.com>
2015-05-29 10:53:03 +0800

    配置环境。

--- cs229/wsgi.py
+++ cs229/wsgi.py
@@ -6,8 +6,12 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
-
+import sys
 import os
+if not os.path.dirname(__file__) in sys.path[:1]:
+    sys.path.insert(0, os.path.dirname(__file__))
+# sys.path.append(os.path.abspath(os.path.dirname(__file__)))
+# sys.path.append('/home/long/www')
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cs229.settings")
 
 from django.core.wsgi import get_wsgi_application
